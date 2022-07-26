d3720722af678c2da7831de99722abf7be162e48
Jerry <jerryyoung2911@gmail.com>
2022-07-26 14:22:11 +0700

    Initialize database

--- infrastructure/database/mysqldriver.go
+++ infrastructure/database/mysqldriver.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jackthepanda96/learn-deploy/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(cfg *config.AppConfig) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", cfg.Username, cfg.Password, cfg.Address, cfg.Port, cfg.Name)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Cannot connect to DB")
+	}
+
+	return db
+}
